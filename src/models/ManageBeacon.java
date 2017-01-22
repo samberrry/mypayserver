a5011e5cfc45a737051def10a8c8aa58f1bf16e3
Hessam Modaberi <hessam.modaberi@gmail.com>
2017-01-22 22:08:28 +0330

    Beacon Location fix

--- src/models/ManageBeacon.java
+++ src/models/ManageBeacon.java
@@ -2,10 +2,7 @@ package models;
 
 import mypaydbmap.BeaconEntity;
 import mypaydbmap.StoreEntity;
-import org.hibernate.Criteria;
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import org.hibernate.Transaction;
+import org.hibernate.*;
 import org.hibernate.criterion.Criterion;
 import org.hibernate.criterion.LogicalExpression;
 import org.hibernate.criterion.Restrictions;
@@ -31,16 +28,26 @@ public class ManageBeacon {
             tx = session.beginTransaction();
 //            CriteriaBuilder builder = session.getCriteriaBuilder();
 //            CriteriaQuery<BeaconEntity> criteria = builder.createQuery(BeaconEntity.class);
-            Criteria cr = session.createCriteria(BeaconEntity.class)
+            /*Criteria cr = session.createCriteria(BeaconEntity.class)
                     .add(Restrictions.disjunction()
                             .add(Restrictions.eq("uuid", uuid))
                             .add(Restrictions.eq("major", major))
                             .add(Restrictions.eq("minor", minor))
-                    );
-            List<BeaconEntity> results = cr.list();
-            for (BeaconEntity beacon :
+                    );*/
+
+//            List<BeaconEntity> results = cr.list();
+
+            String hql = "select beacons.idstore from BeaconEntity beacons where (beacons.major= :major) AND (beacons.minor= :minor) AND (beacons.uuid= :uuid)";
+            org.hibernate.query.Query query = session.createQuery(hql);
+            query.setParameter("uuid",uuid);
+            query.setParameter("major",major);
+            query.setParameter("minor",minor);
+
+            List<Integer> results = query.list();
+
+            for (Integer storeid :
                     results) {
-                 storeID = beacon.getIdstore();
+                 storeID = storeid;
             }
             tx.commit();
             if(storeID != null) {
